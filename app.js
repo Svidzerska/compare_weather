e7f861a893f7117f79157eed9991c397ca2b9c87
Anastasiia Svidzerska <anastasiia_kharchenko@ukr.net>
2021-11-29 18:50:30 +0200

    mongodb was linked, post and get method was added

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 //main js
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectId;
+const { ObjectId } = require('bson');
+
+
 
 const app = express();
 
+//for name properties read
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.use(express.static(__dirname + '/public'));
+
+
+let db;
+const url = 'mongodb://localhost:27017';
+const dbName = 'GoWeather'; 
+const client = new MongoClient(url);
 
 
 app.get('/', (req, res) => {
@@ -11,4 +27,36 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(3030, () => console.log("API started"));
+app.get('/cities', (req, res) => {
+   db.collection('cities').find().toArray((err,docs) => {
+      if(err) {
+         console.log(err);
+         return res.sendStatus(500);
+      }
+      res.send(docs);
+   })
+});
+
+
+app.post('/cities', (req,res) => {
+   let city = {
+      id: Date.now(),
+      name: req.body.name
+   };
+   db.collection('cities').insertOne(city, (err) => {
+      if(err) {
+         console.log(err);
+         return res.sendStatus(500);
+      }
+      res.send(city);
+   })
+})
+
+
+client.connect(err => {
+   if(err) throw err;
+   console.log("connected to server");
+   db = client.db(dbName);
+   app.listen(3030, () => console.log("API started"));
+})
+
