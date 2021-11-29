fa0fa565cb19a75860d40759d317a995706d9e95
Anastasiia Svidzerska <anastasiia_kharchenko@ukr.net>
2021-11-29 16:15:51 +0200

    weather from geolocation was render

--- public/script.js
+++ public/script.js
@@ -6,7 +6,10 @@ document.addEventListener("DOMContentLoaded", () => {
          this.input = document.createElement("input");
          this.addButton = document.createElement("button");
          this.addButton.setAttribute("id","add_city");
+         this.divGeo = document.createElement("div");
+         this.divGeo.setAttribute("class","geoFixed");
          this.mainDiv = document.querySelector(".main");
+         
       }
    
       renderInit() {
@@ -19,18 +22,49 @@ document.addEventListener("DOMContentLoaded", () => {
       clearInput() {
          this.input.value = "";
       }
+
+      addWeatherGeo(temperature,feel_temperature,cloud,precipitation,icon,city) {
+         this.mainDiv.appendChild(this.divGeo);
+         this.divGeo.innerHTML = `<p>${temperature}</p>
+         <p>${feel_temperature}</p>
+         <p>${cloud}</p>
+         <p>${precipitation}</p>
+         <p>${icon}</p>
+         <p>${city}</p>`;
+      }
    }
 
    class WeatherModel {
       constructor(view){
          this.cities = [];
          this.view = view;
+         // this.getCityGeo = this.getCityGeo.bind(this);
+         this.getWeatherGeo = this.getWeatherGeo.bind(this);
       } 
 
       addCityToDB(value) {
          this.cities.push(value);
          console.log(this.cities);
       }
+
+      
+      async getWeatherGeo(latitude,longitude) {
+         try {
+            let result = await fetch('http://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid=18403b04ed7c3c2c59d89a2a42ba33c0');
+            let json = await result.json();
+            let temp = json.main.temp;
+            let feel_temp = json.main.feels_like;
+            let cloud = json.clouds;
+            let precipitation = json.weather[0].description;
+            let icon = json.weather[0].icon;
+            let city = json.name;
+            let weather = [temp,feel_temp,cloud,precipitation,icon,city];
+            return weather;
+         } catch(err) {
+             return err;
+         }
+      }
+
    }
 
 
@@ -52,23 +86,6 @@ document.addEventListener("DOMContentLoaded", () => {
          this.view.addButton.addEventListener("click", this.addCity);
       }
 
-      // getCityGeo(latitude,longitude) {
-      //    $.ajax({ url:'http://api.openweathermap.org/geo/1.0/reverse?lat='+latitude+'&lon='+longitude+'&limit=2&appid=18403b04ed7c3c2c59d89a2a42ba33c0',
-      //       success: function(data){
-      //       console.log(data);
-      //       }
-      //    });
-      // }
-
-      getCityGeo(latitude,longitude) {
-         $.ajax({ url:'http://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid=18403b04ed7c3c2c59d89a2a42ba33c0',
-            success: function(data){
-            console.log(data);
-            }
-         });
-      }
-
-
       getGeo() {
          let geo = navigator.geolocation;
          console.log(geo);
@@ -76,10 +93,13 @@ document.addEventListener("DOMContentLoaded", () => {
             const latitude  = pos.coords.latitude;
             const longitude = pos.coords.longitude;
             console.log(pos.coords);
-            this.getCityGeo(latitude,longitude);
+            this.model.getWeatherGeo(latitude,longitude)
+                        .then(result => result instanceof Error ? 
+                        console.log(result) : 
+                        this.view.addWeatherGeo(result[0],result[1],result[2],result[3],result[4],result[5],result[6]));                                       
             geo.clearWatch(id);
-         }, (err) => console.log(err));
-      }
+         }, (err) => console.log(new Error(err)));
+      }   
    }
 
    function start(){
@@ -94,6 +114,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
    start();
 
+   
 
 
 
